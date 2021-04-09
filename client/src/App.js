5c3eb2484d5e0d1f23592bdce0201fc99a48a0d0
Aliaksandr Svirzheuski <A.Svirzheuski@itechart-group.com>
2021-04-09 19:16:20 +0300

    04/09/2021 passed video 49

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,12 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboadr";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
+import PrivateRout from "./components/routing/PrivateRout";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 import "./App.css";
@@ -32,6 +38,23 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRout exact path="/dashboard" component={Dashboard} />
+              <PrivateRout
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+              <PrivateRout exact path="/edit-profile" component={EditProfile} />
+              <PrivateRout
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRout
+                exact
+                path="/add-education"
+                component={AddEducation}
+              />
             </Switch>
           </section>
         </Fragment>
